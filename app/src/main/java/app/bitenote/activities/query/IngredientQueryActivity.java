2b5664d38d8a93f4f8ce6beb01118768f3d52884
Daniel <danielnaval02.contact@gmail.com>
2025-04-07 20:26:54 +0200

    Merge inclusion and banning activities into one

---
+++ app/src/main/java/app/bitenote/activities/query/IngredientQueryActivity.java
@@ -0,0 +1,18 @@
+package app.bitenote.activities.query;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.bitenote.R;
+
+/**
+ * Class that represents the activity where the user specifies which ingredients the recipes of the
+ * query must have or must not have.
+ */
+public final class IngredientQueryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ingredient_query_activity);
+    }
+}
---
+++ app/src/main/java/app/bitenote/activities/query/UtensilQueryActivity.java
@@ -0,0 +1,18 @@
+package app.bitenote.activities.query;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.bitenote.R;
+
+/**
+ * Class that represents the activity where the user specifies which utensils the recipes of the
+ * query must have or must not have.
+ */
+public class UtensilQueryActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.utensil_query_activity);
+    }
+}
