b9600dc40831f2e01d10d069c2f53eff9a81bb27
Daniel <danielnaval02.contact@gmail.com>
2025-06-02 03:13:54 +0200

    Add example recipe insertion on first app run

--- app/src/main/java/app/bitenote/app/BiteNoteApplication.java
+++ app/src/main/java/app/bitenote/app/BiteNoteApplication.java
@@ -1,7 +1,12 @@
 package app.bitenote.app;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Looper;
 import androidx.lifecycle.ViewModelProvider;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import app.bitenote.viewmodels.BiteNoteViewModel;
 
@@ -9,6 +14,16 @@ import app.bitenote.viewmodels.BiteNoteViewModel;
  * Class that represents the BiteNote application (added in the manifest).
  */
 public class BiteNoteApplication extends Application {
+    /**
+     * Name of app shared preferences.
+     */
+    private static final String PREFS_NAME = "bitenote_prefs";
+
+    /**
+     * Shared preference name for checking if the app is on its first run.
+     */
+    private static final String PREF_IS_FIRST_RUN = "is_first_run";
+
     /**
      * Application scoped view model. Grants access to the database API and shared live data.
      */
@@ -20,6 +35,21 @@ public class BiteNoteApplication extends Application {
 
         mAppViewModel = new ViewModelProvider.AndroidViewModelFactory(this)
                 .create(BiteNoteViewModel.class);
+
+        final Executor databaseExecutor = Executors.newSingleThreadExecutor();
+        final Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        final SharedPreferences sharedPrefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        final boolean isFirstRun = sharedPrefs.getBoolean(PREF_IS_FIRST_RUN, true);
+        if (isFirstRun) {
+            databaseExecutor.execute(() -> {
+                mAppViewModel.sqliteHelper.insertExampleRecipes();
+
+                mainThreadHandler.post(() ->
+                        sharedPrefs.edit().putBoolean(PREF_IS_FIRST_RUN, false).apply()
+                );
+            });
+        }
     }
 
     /**
